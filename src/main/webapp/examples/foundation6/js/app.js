03b68058a9be070b59513d325e0f7f6903ae9434
zhy2002 <hz141@uowmail.edu.au>
2016-01-21 17:16:39 +1100

    Added transition tests.

--- src/main/webapp/examples/foundation6/js/app.js
+++ src/main/webapp/examples/foundation6/js/app.js
@@ -1,1 +1,36 @@
 $(document).foundation();
+
+
+function isScrolledIntoView(elem) {
+    var $elem = $(elem);
+    var $window = $(window);
+
+    var docViewTop = $window.scrollTop();
+    var docViewBottom = docViewTop + $window.height();
+
+    var elemTop = $elem.offset().top;
+    var elemBottom = elemTop + $elem.height();
+
+    return ((elemBottom <= docViewBottom) && (elemTop >= docViewTop));
+}
+
+$(function(){
+    var myBox = $('#myBox');
+    $('#callWindow').click(function(){
+        myBox.toggleClass('box-show');
+    });
+
+    myBox.toggleClass('box-show');
+    $(document).scroll(function(){
+        if(!myBox.hasClass('box-show')) {
+            if(isScrolledIntoView(myBox)){
+                myBox.toggleClass('box-show');
+            }
+        }
+    });
+
+
+
+
+});
+
